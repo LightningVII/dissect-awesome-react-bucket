abb6ae9df9f0bec883be8343f11bfc5b158c9f53
WhiteAce <whiteace@yeah.net>
2017-09-11 11:17:40 +0800

    styled-components 封装写法修改

--- src/style/common.tsx
+++ src/style/common.tsx
@@ -1,16 +1,9 @@
-import * as React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-const LinkStyle = styled(NavLink)`
+const Link = styled(NavLink)`
   display: block;
   text-decoration: none;
 `;
-
-const Link = (props: any) => (
-  <LinkStyle {...props} >
-    {props.children}
-  </LinkStyle>
-)
 
 export {
   Link
