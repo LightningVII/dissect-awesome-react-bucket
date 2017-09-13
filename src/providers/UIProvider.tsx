5bc37876cfe2508fb498f3c64380e2eaee619831
WhiteAce <whiteace@yeah.net>
2017-09-13 11:32:16 +0800

    tslint

--- src/providers/UIProvider.tsx
+++ src/providers/UIProvider.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import '../icomoon/style.css'
-import { MuiThemeProvider, getMuiTheme } from "material-ui/styles";
+import '../icomoon/style.css';
+import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
 import { blue500, grey600 } from 'material-ui/styles/colors';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -19,7 +19,7 @@ const MyUIProvider = (props: any) => {
         <MuiThemeProvider muiTheme={muiTheme}>
             {props.children}
         </MuiThemeProvider>
-    )
-}
+    );
+};
 
 export default MyUIProvider;
