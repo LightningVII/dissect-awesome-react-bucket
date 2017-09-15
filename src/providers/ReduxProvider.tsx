835660beca30c62ee89ee4fbb0c41fa4d1356ab6
WhiteAce <whiteace@yeah.net>
2017-09-15 10:52:29 +0800

    yarn add react-addons-perf @types/react-addons-perf

--- src/providers/ReduxProvider.tsx
+++ src/providers/ReduxProvider.tsx
@@ -1,7 +1,7 @@
 'use strict';
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import configureStore from './stores';
+import configureStore from '../common/stores';
 const store = configureStore();
 const ReduxProvider = (props: any) => {
     return <Provider store={store}>{props.children}</Provider>;
