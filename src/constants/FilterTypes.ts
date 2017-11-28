8586c542ac7c2b33af5f52ecf12fdf2b6816eaa7
WhiteAce <whiteace@yeah.net>
2017-11-28 10:47:39 +0800

    remove redundance code

--- src/constants/FilterTypes.ts
+++
@@ -1,7 +0,0 @@
-const FilterTypes = {
-    ALL: '全部',
-    COMPLETED: '已完成',
-    UNCOMPLETED: '未完成'
-};
-
-export default FilterTypes;
--- src/constants/Routers.ts
+++
@@ -1,2 +0,0 @@
-const routers = ['/', '/list', '/links'];
-export default routers;
